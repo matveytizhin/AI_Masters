f099aba8b273ba383fb5cbc013e9dd497dcf9153
Тижин_Матвей <matveytizhin@edge1.ru-central1.internal>
2025-10-14 16:46:50 +0000

    new

--- LINUX_BASH_SSH/program.py
+++ LINUX_BASH_SSH/program.py
@@ -1,7 +1,10 @@
 print("Hello World")
 # program.py
+print("work start")
 def greet():
     print("Hello, world!")
+    name = "matveytizhin"
+    print(name)
 for i in range:
   if x == 5:
 	print('5')
